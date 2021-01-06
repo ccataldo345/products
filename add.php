1a6f2f9cd324381d4d5b6cc7203d9a2724b5f732
Chris C <epost345@gmail.com>
2021-01-06 20:23:38 +0200

    Add form input validation on server side

--- add.php
+++ add.php
@@ -1,7 +1,7 @@
 <?php
-include "components/database.php";
 include "components/header.php";
-include "components/save_product.php"
+include "components/database.php";
+include "components/save_product.php";
 ?>
 
 <div class="container mt-3">
@@ -21,36 +21,39 @@ include "components/save_product.php"
 <div class="container">
 
   <div class="col-8 my-3">
-    <form id="add_form" action="add.php" method="post" enctype="multipart/form-data">
+    <form id="add_form" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" enctype="multipart/form-data">
       <div class="form-group row">
         <label for="sku" class="col-sm-2 col-form-label">SKU</label>
         <div class="col-sm-6">
-          <input class="form-control" type="text" name="sku" required>
+          <input class="form-control" type="text" name="sku" value="<?php echo $sku;?>" required>
         </div>
+        <span class="error"><?php echo $skuErr;?></span>
       </div>
       <div class="form-group row">
         <label for="name" class="col-sm-2 col-form-label">NAME</label>
         <div class="col-sm-6">
-          <input class="form-control" type="text" name="name" required>
+          <input class="form-control" type="text" name="name" value="<?php echo $name;?>" required>
         </div>
+        <span class="error"><?php echo $nameErr;?></span>
       </div>
       <div class="form-group row">
         <label for="price" class="col-sm-2 col-form-label">PRICE ($)</label>
         <div class="col-sm-6">
-          <input class="form-control" type="number" min="0.01" max="10000.00" step="0.01" name="price" required>
+          <input class="form-control" type="number" min="0.01" max="10000.00" step="0.01" name="price" value="<?php echo $price;?>" required>
         </div>
-      </div>
+        <span class="error"><?php echo $priceErr;?></span>
+      </div>required
       <div class="form-group row">
         <label for="image" class="col-sm-3 col-form-label">Choose picture</label>
         <div class="col-sm-5">
-          <input type="file" class="form-control-file" name="img" id="img">
+          <input type="file" class="form-control-file" name="img" value="<?php echo $img;?>" id="img">
         </div>
       </div>
       <div class="form-group row">
         <label for="typeswitcher" class="col-sm-3 col-form-label">Type Switcher</label>
         <div class="col-sm-5">
-          <select class="form-control" name="type" id="type_switcher">
-            <option selected>Type Switcher</option>
+          <select class="form-control" name="type" id="type_switcher" required>
+            <option selected value="">Type Switcher</option>
             <option value="1">1 - DVD-disc</option>
             <option value="2">2 - Book</option>
             <option value="3">3 - Furniture</option>
--- components/save_product.php
+++ components/save_product.php
@@ -1,76 +1,80 @@
 <?php
 
-if (isset($_POST["save"])) {
+require_once "components/validation.php";
 
-  $conn = new mysqli(DB_SERVER, DB_USER, DB_PASS);
-  $conn->select_db(DB_NAME);
+if (!$form_errors) {
+  if (isset($_POST["save"])) {
 
-  // image upload
-  $target_dir = "img/";
-  $target_file = $target_dir . basename($_FILES["img"]["name"]);
-  $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
-  $uploadOk = 1;
+    $conn = new mysqli(DB_SERVER, DB_USER, DB_PASS);
+    $conn->select_db(DB_NAME);
 
-  // // Check file size < 500 Kb
-  // if ($_FILES["fileToUpload"]["size"] > 500000) {
-  //   echo "Sorry, your file is too large.";
-  //   $uploadOk = 0;
-  // }
+    // image upload
+    $target_dir = "img/";
+    $target_file = $target_dir . basename($_FILES["img"]["name"]);
+    $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+    $uploadOk = 1;
 
-  if ($uploadOk == 1) {
-    if (!move_uploaded_file($_FILES["img"]["tmp_name"], $target_file)) {
-      echo "Sorry, there was an error uploading your file. ";
-      echo $_FILES['img']['error'];
+    // // Check file size < 500 Kb
+    // if ($_FILES["fileToUpload"]["size"] > 500000) {
+    //   echo "Sorry, your file is too large.";
+    //   $uploadOk = 0;
+    // }
+
+    if ($uploadOk == 1) {
+      if (!move_uploaded_file($_FILES["img"]["tmp_name"], $target_file)) {
+        echo "Sorry, there was an error uploading your file. ";
+        echo $_FILES['img']['error'];
+      }
     }
+
+    $sku = $_POST['sku'];
+    $name = $_POST['name'];
+    $price = $_POST['price'];
+    $img = "img/" . $_FILES['img']['name'];
+    $type = $_POST['type'];
+    $size = $_POST['size'];
+    $weight = $_POST['weight'];
+    $height = $_POST['height'];
+    $width = $_POST['width'];
+    $length = $_POST['length'];
+
+    $stmt = $conn->prepare("INSERT INTO " . PRODUCTS_TABLE . " (SKU, name, price, img, type) VALUES (?, ?, ?, ?, ?)");
+    if (!$stmt) die("Prepare failed: (" . $conn->errno . ") " . $conn->error);
+    $stmt->bind_param("ssdsi", $sku, $name, $price, $img, $type);
+    if (!$stmt->execute()) {
+      die("Execute failed: (" . $stmt->errno . ") " . $stmt->error);
+    }
+    $stmt->close();
+
+    $product_id = $conn->insert_id;
+
+    switch ($type) {
+      case "1":
+        $new_type = "dvds";
+        $stmt = $conn->prepare("INSERT INTO " . $new_type . " VALUES (?, ?)");
+        if (!$stmt) die("Prepare failed: (" . $conn->errno . ") " . $conn->error);
+        $stmt->bind_param("id", $product_id, $size);
+        break;
+      case "2":
+        $new_type = "books";
+        $stmt = $conn->prepare("INSERT INTO " . $new_type . " VALUES (?, ?)");
+        if (!$stmt) die("Prepare failed: (" . $conn->errno . ") " . $conn->error);
+        $stmt->bind_param("id", $product_id, $weight);
+        break;
+      case "3":
+        $new_type = "furnitures";
+        $stmt = $conn->prepare("INSERT INTO " . $new_type . " VALUES (?, ?, ?, ?)");
+        if (!$stmt) die("Prepare failed: (" . $conn->errno . ") " . $conn->error);
+        $stmt->bind_param("iddd", $product_id, $height, $width, $length);
+        break;
+    }
+    if (!$stmt->execute()) {
+      die("Execute failed: (" . $stmt->errno . ") " . $stmt->error);
+    }
+    $stmt->close();
+
+    $conn->close();
+
+    header("Location:index.php");
   }
-
-  $sku = $_POST['sku'];
-  $name = $_POST['name'];
-  $price = $_POST['price'];
-  $img = "img/" . $_FILES['img']['name'];
-  $type = $_POST['type'];
-  $size = $_POST['size'];
-  $weight = $_POST['weight'];
-  $height = $_POST['height'];
-  $width = $_POST['width'];
-  $length = $_POST['length'];
-  
-  $stmt = $conn->prepare("INSERT INTO " . PRODUCTS_TABLE . " (SKU, name, price, img, type) VALUES (?, ?, ?, ?, ?)");
-  if (!$stmt) die("Prepare failed: (" . $conn->errno . ") " . $conn->error);
-  $stmt->bind_param("ssdsi", $sku, $name, $price, $img, $type);
-  if (!$stmt->execute()) {
-    die("Execute failed: (" . $stmt->errno . ") " . $stmt->error);
-  }
-  $stmt->close();
-
-  $product_id = $conn->insert_id;  
-
-  switch ($type) {
-    case "1":
-      $new_type = "dvds";    
-      $stmt = $conn->prepare("INSERT INTO " .$new_type . " VALUES (?, ?)");
-      if (!$stmt) die("Prepare failed: (" . $conn->errno . ") " . $conn->error);
-      $stmt->bind_param("id", $product_id, $size);  
-      break;
-    case "2":
-      $new_type = "books";
-      $stmt = $conn->prepare("INSERT INTO " .$new_type . " VALUES (?, ?)");
-      if (!$stmt) die("Prepare failed: (" . $conn->errno . ") " . $conn->error);
-      $stmt->bind_param("id", $product_id, $weight);
-      break;
-    case "3":
-      $new_type = "furnitures";
-      $stmt = $conn->prepare("INSERT INTO " .$new_type . " VALUES (?, ?, ?, ?)");
-      if (!$stmt) die("Prepare failed: (" . $conn->errno . ") " . $conn->error);
-      $stmt->bind_param("iddd", $product_id, $height, $width, $length);
-      break;
-  }
-  if (!$stmt->execute()) {
-    die("Execute failed: (" . $stmt->errno . ") " . $stmt->error);
-  }
-  $stmt->close();
-
-  $conn->close();
-
-  header("Location:index.php");
 }
